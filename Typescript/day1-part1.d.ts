dce3fca4c86116b1c6838e20ac1dcec190aec2f0
Dante De Ruwe <dante.deruwe@ae.be>
2024-12-04 15:54:56 +0100

    Add day one solutions fully in typescript types

---
+++ Typescript/day1-part1.d.ts
@@ -0,0 +1,189 @@
+// SEE ALSO AT TYPESCRIPT PLAYGROUND: https://www.typescriptlang.org/play/?#code/PTAEFkFMCcHNNAZwPbQC6gMbICYIEQBGq0yA7pDvqAGakC2oAFmmgA6IBcIeAbpABtkbGIgB09SGXHZ6wWWwCWAyAFo0iyapoBXAHaYNyPQEMBqtqVjQT9eor2xVD9QE8RiTNEVs0wAFAgoACqeorGACrIETpsKli4BMTQpBRUzKwc3MBkuWJMJpgA1ipkJq56kGKywGjukJ7evqrYevzoqvrheurI6rEqqiYp5aquDaqEOmiqTOT+-nUioFEAcjr0hDAAPACCoJAAHmiQejiIoPpFeuR6ANoAugB8oAC8oLt3AOQqjmhMXweAG4FksEABhZD6NDbVYHY6nc6gPQbLbQAA0H3hJzOFyuNzI9web1Ajxe70+P1OsH+gOxiIuq38oFAAH4PszQJxQJDobDMXcxELdpj9HgaA5KM8QYt6qAAPoASQM2wi9NxyNRMHJpKFYl5ehhESemIADA9vr8aQDgbLlsrMKr1UiUZttSSlSrjc6Lq60WyFQ7VS9uZV2jKwaAACKQR1qo44l1a6A6g1Gl4Jhm6oUOGgwD6is6QCWVHAPTnsylW2nllmhyDh0FygDKGz2Ps1boxoAAQh2-e73mm9hmERrBTm9HnoB8AIy1gPDnujxMXCdiXP5nvzzks9nr3azzF67cWql-G27rnIhswTn1xt2hC7EauVv0duZjUD6Bkkn7L8kTuTcZwAMUUaBEDQY9J2nUAACUGjQBd2XAyCMEA31kyvdlEKg-tk0eK89w+V9322PC0BXLMf2IlkA3ItCoMxF8bDfNtKKeJ46LrG92joh873o68wyE69TQjOUo0UGgaE-Mcky7TE+0wzs0R1FSFIuU0Kw5XjpNk7YY0dXYTWjWNtmXJ5JOWXZCEQAy5IArS1JgZSCK7HUAHFoEgEwTmgPZlOojU0GgHRIF0xygt7bj9JkuSexY6ym2WZswocWBdkQdYu1VTF4I8-13lNHV4xcgADAASABveCAF8Kt0+D71AdLvEcbLcrRfLQCDeCuJshB5R8vyAvk1dLj0a5bkedzVPxWbnn-Ds7k5PUQLndFOTFYtJRwfwUN7VaNqnLcjym8V9oXEiRt8-ydkPZTZzi+juTCiLWpoMxEEgIbQFGh7Aucyaf3mlyfx1O6xp2YdTMxJdBtShBmziRQ0HazLtk5ZsOygjrYG2llm0gNhhn81A8Yyxx-B1XHVOqmrNtA+rapJsmbDQVBWaZs6ZwapqSLuUCYLEVGBHRzHHAozF2fJrmUwXbk7mbW1AjAKA4EgbY7gugAmTEAFZMQAdgeAUAGZMQAFkxAA2ZbVBeXXMQN0ArdAW3QGN0A7dN8tI01+AJpowjzeO1Sfz-ClVs5ZmIKgkV1tg-NKN2Q7dM0ybgL50AmLQJLQFOuDKJ7G6A3zkHQ67OjcKQqvvzDniK4TguirE4ThLrqCs+rtEiM7weA0B8bK8xfPlw7b6BF+5vB-3Mei6FIPtbT4Ly6Ht7SQn0WV5i0ung3oflaP4ST+b8-B8vrf094nsZSCcX0dY8odf1zEPa9n2-dAM2Xid0kLtQBu0-uHO439-YBxbGjNAL9XDYxZA3RS-dzacgADLt1-MSd4c1OSFUjmHEkREdRILXPHdCotNqUUxJtNBJgoJHXzpgnCCEkKYIHp3dkdD8IEJrpvdkT9YGvgokhAUeo0Hj1bmA7h0El5iHgs8C+gCwEPCPsrOapJVFXnUSopWWJVI5zgvnI6dx1wSLzlIgqei7jmIUf9FezZUAwlIa5LBdMYFwJHKtWhNDc4KN0hg3hKCWH4Ihk3eeCFLTUhrB2HS-DQBoKUXvBxTjthoLMik9AFEuICT4h3ESt5oCtVEtAB+YAIg2D0IgNgyBfo6xdnrMBVtrZgONg7R2zsXZW0NmAg2tt2lPhWJU6ptTtYuMWoSR40dQA1U5HcAA0qABwoAiiQFcMgGgHwHjclmcJBZesll6BWWsjZWzuSfHmY0hZtoWT1RBHc5GoAAAKJgIIAHVFC-Tsg5BKCD9FhK7LglkvdG6AtQSyUJoNCE4PLCQ7xudF7F1TvXRhrcXG0S7qwxO7Cj7shBUBchPdKF+KQmXZuqFW74qwnwzeAZS44rnvRdkLz3mfMgN86Ka8sUFzEUKeCLF7LRUXhPQ+r1aXXhauK7kkrj4IVyTK3iMrpVlLatTWA0lYDP1fGsZMiB6n4AtvgTE+Brb4DASao1oADVmoFPgPWlr8CGxtaSfAs4HWGvNYa41ABOZ1dxrXGo9Y7ABpjQECi9hbXpRswEXUjZbTE3qmkfy0ZGKW6rFCarQCyyCOqux6s5OVSa+NMpTPBQhHFjwiGwpJIWrMhj8wizkVQpCR1KJUwJqWlhoF4VGIuszRptc86znbZlclecDmqWLTTcVAi1UavRtmxAua0T5vFcJahjK9l6n5aSNNXVkzbFAi9WWar915VAnrQ+p9O5itlQqzu0r5XaLlYq-6kJDQ2GDDWjsJg9CuADAACmGLAbkEQACUbwXi8GQIoHABTGyRkhLwL97xa0al-f+9kAGIOvBeGqQSpTBnKmnEhlDpIIjEgMdhyDhy4KKmJOyRU8GYAgkjMRmA76womDI3cCjq0gNwG5JtRUOGoMwbLAGJjBHWNyieYoYoAB5SoZH2PQE45+1T6nuNae2Dp40XEUqRlCN0KIMQ4ja29O8OTinlPelUptYIukACihxMACB0HgXqwQQoukKSw3j5dlZ6mM5EaIAxtYubcx5izmJvNmWCHokpZTVCqCI3oNg0wSQVQtvRa2-hrb0Qtv4A5LJDb+GHSyIrOWWTev8NV92FV-poMlBcd4gi03bGVBl2RXwAA6egviGblN1UQ2U00haOW1mBHXmuVEQHcH84cvj0UGzJ5YI3oBptayEMIoWzMqFhLqsbaqJtDfW7qkkab51ZteTm5AG29Uba2ylQZFTf2ICeaMuDqHhk1LqQ9l7qanGUDQcWDA7xMlGmGZ936OA7jmjOyjYHOB4IZpYCSSHqpodfd1tKR5jltvZo+V8wVvzHHoBB2D2WyPUewBYIjrFOgBDg9ImxciBOUpBBZAAPVZP4IAA
+
+// Credits:
+// - Merge sort code "borrowed" from https://developers.mews.com/compile-time-functional-programming-in-typescript/
+// - UnionToTuple code "borrowed" https://www.hacklewayne.com/typescript-convert-union-to-tuple-array-yes-but-how
+// - Rest is my own work ^-^
+
+type ToNumber<A extends unknown[]> = A['length'];
+
+type Count<N extends number, A extends unknown[] = []> = A['length'] extends N
+  ? A
+  : Count<N, [...A, undefined]>;
+
+type _Inc<T extends number> = [...Count<T>, 0]['length'];
+type Inc<T extends number> = _Inc<T> extends number ? _Inc<T> : never;
+
+type Dec<T extends number> = Count<T> extends [...infer A, undefined]
+  ? A['length']
+  : never;
+
+type Sum<A extends number, B extends number> = Count<A> extends [...infer A1]
+  ? Count<B> extends [...infer B1]
+    ? [...A1, ...B1]['length']
+    : never
+  : never;
+
+type ArraySum<A extends number[]> = A extends [infer First, ...infer Rest]
+  ? First extends number
+    ? Rest extends number[]
+      ? ArraySum<Rest> extends number
+        ? Sum<First, ArraySum<Rest>>
+        : never
+      : never
+    : never
+  : 0;
+
+type Diff<A extends number, B extends number> = B extends 0
+  ? A
+  : Diff<Dec<A>, Dec<B>>;
+
+type AbsDiff<A extends number, B extends number> = Greater<A, B> extends true
+  ? Diff<A, B>
+  : Diff<B, A>;
+
+type StringAsNumber<T, R extends number = 0> = T extends `${R}`
+  ? R
+  : StringAsNumber<T, Inc<R>>;
+
+type _Greater<A extends unknown[], B extends unknown[]> = A extends [
+  ...infer A1,
+  undefined
+]
+  ? B extends [...infer B1, undefined]
+    ? _Greater<A1, B1>
+    : true
+  : false;
+
+type Greater<A extends number, B extends number> = _Greater<Count<A>, Count<B>>;
+
+type SplitString<
+  S extends string,
+  Separator extends string
+> = S extends `${infer F}${Separator}${infer R}`
+  ? [F, ...SplitString<R, Separator>]
+  : [S];
+
+// Merge<[1, 2, 5, 7], [3, 4, 6]> -> [1, 2, 3, 4, 5, 6, 7]
+type Merge<A extends number[], B extends number[]> = A extends [
+  infer FirstA,
+  ...infer RestA
+]
+  ? B extends [infer FirstB, ...infer RestB]
+    ? FirstA extends number
+      ? RestA extends number[]
+        ? FirstB extends number
+          ? RestB extends number[]
+            ? Greater<FirstA, FirstB> extends false
+              ? [FirstA, ...Merge<RestA, B>]
+              : [FirstB, ...Merge<A, RestB>]
+            : []
+          : []
+        : []
+      : []
+    : A
+  : B;
+
+// SplitArray<[1, 2, 3, 4, 5, 6, 7]> -> [[1, 2, 3, 4], [5, 6, 7]]
+type SplitArray<
+  A extends number[],
+  L extends number[] = [],
+  R extends number[] = []
+> = A extends [infer First, ...infer Rest, infer Last]
+  ? First extends number
+    ? Rest extends number[]
+      ? Last extends number
+        ? SplitArray<Rest, [...L, First], [Last, ...R]>
+        : [[], []]
+      : [[], []]
+    : [[], []]
+  : A extends [infer First]
+  ? [[...L, First], R]
+  : [L, R];
+
+type MergeSort<A extends number[]> = SplitArray<A> extends [infer L, infer R]
+  ? L extends number[]
+    ? R extends number[]
+      ? R['length'] extends 0
+        ? L
+        : Merge<MergeSort<L>, MergeSort<R>>
+      : never
+    : never
+  : never;
+
+// Transpose<[[1,2], [3,4], [5,6]]> -> [[1,3,5], [2,4,6]]
+type Transpose<A extends unknown[][]> = {
+  [K in keyof A]: {
+    [K2 in keyof A]: A[K2][K]; // TS will complain but it will still work
+  };
+};
+
+type PairWiseAbsDiff<
+  A extends number[],
+  B extends number[],
+  R extends number[] = []
+> = A extends [infer FirstA, ...infer RestA]
+  ? FirstA extends number
+    ? RestA extends number[]
+      ? B extends [infer FirstB, ...infer RestB]
+        ? FirstB extends number
+          ? RestB extends number[]
+            ? PairWiseAbsDiff<RestA, RestB, [...R, AbsDiff<FirstA, FirstB>]>
+            : R
+          : R
+        : R
+      : R
+    : R
+  : R;
+
+// StringDigitArrayToNumbers<[["3", "4"], ["4", "3"], ["2", "5"], ["1", "3"], ["3", "9"], ["3", "3"]]> --> [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]]
+type StringDigitPairsToNumbers<
+  T extends string[][],
+  R extends number[][] = []
+> = T extends [infer F, ...infer Rest]
+  ? Rest extends string[][]
+    ? F extends [infer F1, infer F2]
+      ? F1 extends string
+        ? F2 extends string
+          ? StringDigitPairsToNumbers<
+              Rest,
+              [...R, [StringAsNumber<F1>, StringAsNumber<F2>]]
+            >
+          : R
+        : R
+      : R
+    : R
+  : R;
+
+type Contra<T> = T extends any ? (arg: T) => void : never;
+type Cov<T> = T extends any ? () => T : never;
+type InferCov<T> = [T] extends [() => infer I] ? I : never;
+type InferContra<T> = [T] extends [(arg: infer I) => void] ? I : never;
+type PickOne<T> = InferContra<InferContra<Contra<Contra<T>>>>;
+type UnionToTuple<T> = PickOne<T> extends infer U
+  ? Exclude<T, U> extends never
+    ? [T]
+    : [...UnionToTuple<Exclude<T, U>>, U]
+  : never;
+
+//--
+type Input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+type Lines = SplitString<Input, '\n'>;
+type NumbersAsStringUnion = SplitString<Lines[number], '   '>;
+type NumberStrings = UnionToTuple<NumbersAsStringUnion>;
+type Numbers = StringDigitPairsToNumbers<NumberStrings>;
+
+type TransPosed = Transpose<Numbers>;
+
+type SortedLeft = MergeSort<TransPosed[0]>;
+type SortedRight = MergeSort<TransPosed[1]>;
+
+type Diffs = PairWiseAbsDiff<SortedLeft, SortedRight>;
+type Result = ArraySum<Diffs>;
+//   ^? 11
---
+++ Typescript/day1-part2.d.ts
@@ -0,0 +1,138 @@
+// SEE ALSO AT TYPESCRIPT PLAYGROUND: https://www.typescriptlang.org/play/?#code/PTAEGECcFMBMEsAuBnAXAKBKAtKAqgHbwD2BAKsWQK4AOANtKAMbGyMBEARsZJMQO5x2oABaJENNCH4yAdCICGTANYN+CgJ4FoslgFtgiDTWjImkeDUTYWBAG7RI1qkVLZExd7QbYFvTdgapticVNYiAphguABKpoig8MigehqgAgSg-DzKoAB62Hno6EYmoBQAclR6nI4APACCoNAAHojQBLDJLsoEGQDaALoAfKAAvKAN-QDkDAQA5ogi04MA3MWljODELoh1Fc1tHV2gBNW1kAA0k4ftnd0EvQOD46BDoxNTsx2Lyy+tdxOFXQoFAAH5JiDQKgIDsCHsKtd+rIUQ1ri42AAzeDaWAjdYlYyMAD6AEkCEw6mRbsdkmcao4Pm8UbJtrsqcNrgAGQYzOa-FbrTagcmU6kA2mnc6M15kikcmn3KUMyDg0BysWjGHaByQAnCgAi0DFipO9IuTLZ8IVEqVyJROMxjkm6M60GxuMGUIhX35SxWUO10F1+qJoAAytVGqa6dKrqAAEIx5UW15WvYNUa2k722SO50NACMXtBEPTdQTWaOdpZ+dVCeLUNLzNRheuLIbvO+C39JdB0NOwccgcHIY2YeJAFkqHQ6lCmtnYyrLlCk4uU44V6CYsnzc6Jlz0EyF9WTly1eeYWvT8lzxCdzCpzPGtcjZTK9dI3o6jFrpnhqGZTTrOJ6AkuFzXNeYEbpATJPiBkFVtBe6QN66rAS+ibDCOOqOIBjANP4GhftG64oe8rygZK-R1qAABi8CQMgiDtg6BBOqqcTMX2EIMUxCRkXGTZqlxAk3jBQzCc2hGQJoJGiUhkooVJ-YQiRfHMX+RHyfEwzYf2BkDrhqGGUZQ4mf2Qa6iOXL4RGiAWAsDTIFUKpUtcO6CSqrxcky4riQABgAJAA3jEAC+AVoTEI7hg5OLzM5rkXO5IryjEel2eG9BIHFjnzHOoLhsmzH5VuEbQDQfgKB4qrrqVCVHq8xXrsFIW0XR4WheGlXVbVXXtexzoRVFzb9HRrGyNldC5fFCw-p+vWybVwx9jC-ThmsxRYGQskEMgNDEMg0B1P0-RtgATIMSIAMyXAALNdbwAKyXAAbIMIw4KMZ1tndz1Pf0F0Pe9n2EmUu0KPth3HaR4k9H0-AEEMFETCFUL9AA0okmTKNAGjEJikyDDC6MGVjF046AeME0TDQk5MFO8pjaygDtxX8PAdB0MwxB6PQCg4qAoQJEgWRczzzES1kORQuF6zy9tYB5QlBrwPMSAyZolRxsgp39OwN3sNc7D3ewgOm8boCG+bSLsBdVvsM9ttvOwhaO0bFtGybACcLsG971ue192DYD9-R3aAj1Ivd1w3YDwOgADSJtqA8e3dcPuA5H8desKKsLGrGuIAACoLTE6yqetQv50ENQsKPXVCnnieRvIvBMkl+cmNFDaqE2gLWfegKJPEj-EJVzfMjfCbxPcdanHVXSpvGFpP+Uqc2dGU-VU+b6p9n5UXSBl4xyCVxc1emdf4+afvpm5r+bwF4lLlxnUdGFpyh8JUl7-b6tPsN9QT6WATCGKN9wEqSgQZGBA4Yh2W2PCWSCoJi10lFDNIEIAAUfh5gwjIAASnGKMOwxB4CwDMmOYU2w7CoPKMmTBapsHELGKMakVk8Lg0YOSDitD6H9DIP8cS-QWEkJxhxEULwISkioVw4UvDHBIIcgoARQie64MgPgiRzpSSsNIeQvEapZGcL1Nw0AJd4AqAAPLaHoYoyAyiUEOKcao1xdR3FkD0plcxhASDkEoN4E6XjXiWJsXYkJ65aJ4DQgAURaEwOgVA2CpTwIpJUxlZ5vCEcJdaLI-GkAoNQegJ14mJOScE64aTv54DWqOLh5iAAyxBiDKFoMlRw1jMTWKYEwKgMAKSmEKqAZprTaDJgRhkcqMkJmPERsjJu25dxxh8k1T4vB57Dw0ixQebFJGjzQts5Moy2k0CySc9pcYuk9L6QMpgQyLk0A8vEa4oofx6VySMlppyOmQGub0-pHR7l60ec8zSI9QHgMQQoOgfS6A1WgOGFgMAr4jPdGJaCkykZDHKjEdWYhZlPGxYskeyyVRDFeF3V4jT0WbMkY01OQ96UHObAyslFwsngBhXChFSKeBDP3o00S5VTJ4vmGIEVhlH7XAwgy64jzfn-NuUCh5qcxViD0kA-soDQRwKhUrUO5jyQ0DCK8AKN1+z3XQPdfsN10CU1BM9dAa9QR2otaCH26B3VpwCnZRpOJTDNRyogF+dRjVhGuNMAAOgQaYAFzG-OQM5F+BTMgTGmrNfKdR-XaGQP0FCT1pj9jjUKMMvyX7JAmKmopQT9i62TVPVN8bhSJualPY+pdy7n2IImutKoK3xvMZDfaJcjpwFeMOg6Y6+2X2bWGGlmIEhoL2sgUdx1YD9B5KWso6ql3lBXWuuA50trmL5SitM3KZy8uRQ89FHl8WIEHcKLiM491a2IlGM9ph41YFBHkME6AgA
+
+// Credits:
+// - UnionToTuple code "borrowed" https://www.hacklewayne.com/typescript-convert-union-to-tuple-array-yes-but-how
+// - Rest is my own work ^-^
+
+type ToNumber<A extends unknown[]> = A['length'];
+
+type Count<N extends number, A extends unknown[] = []> = A['length'] extends N
+  ? A
+  : Count<N, [...A, undefined]>;
+
+type _Inc<T extends number> = [...Count<T>, 0]['length'];
+type Inc<T extends number> = _Inc<T> extends number ? _Inc<T> : never;
+
+type Dec<T extends number> = Count<T> extends [...infer A, undefined]
+  ? A['length']
+  : never;
+
+type Sum<A extends number, B extends number> = Count<A> extends [...infer A1]
+  ? Count<B> extends [...infer B1]
+    ? [...A1, ...B1]['length']
+    : never
+  : never;
+
+type _Mul<
+  A extends number,
+  B extends number,
+  R extends number = 0
+> = A extends 0 ? 0 : B extends 0 ? R : _Mul<A, Dec<B>, Sum<R, A>>;
+
+type Mul<A extends number, B extends number> = _Mul<A, B> extends number
+  ? _Mul<A, B>
+  : never;
+
+type ArraySum<A extends number[]> = A extends [infer First, ...infer Rest]
+  ? First extends number
+    ? Rest extends number[]
+      ? ArraySum<Rest> extends number
+        ? Sum<First, ArraySum<Rest>>
+        : never
+      : never
+    : never
+  : 0;
+
+type StringAsNumber<T, R extends number = 0> = T extends `${R}`
+  ? R
+  : StringAsNumber<T, Inc<R>>;
+
+type SplitString<
+  S extends string,
+  Separator extends string
+> = S extends `${infer F}${Separator}${infer R}`
+  ? [F, ...SplitString<R, Separator>]
+  : [S];
+
+// Transpose<[[1,2], [3,4], [5,6]]> -> [[1,3,5], [2,4,6]]
+type Transpose<A extends unknown[][]> = {
+  [K in keyof A]: {
+    [K2 in keyof A]: A[K2][K]; // TS will complain but it will still work
+  };
+};
+
+// StringDigitArrayToNumbers<[["3", "4"], ["4", "3"], ["2", "5"], ["1", "3"], ["3", "9"], ["3", "3"]]> --> [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]]
+type StringDigitPairsToNumbers<
+  T extends string[][],
+  R extends number[][] = []
+> = T extends [infer F, ...infer Rest]
+  ? Rest extends string[][]
+    ? F extends [infer F1, infer F2]
+      ? F1 extends string
+        ? F2 extends string
+          ? StringDigitPairsToNumbers<
+              Rest,
+              [...R, [StringAsNumber<F1>, StringAsNumber<F2>]]
+            >
+          : R
+        : R
+      : R
+    : R
+  : R;
+
+type Contra<T> = T extends any ? (arg: T) => void : never;
+type Cov<T> = T extends any ? () => T : never;
+type InferCov<T> = [T] extends [() => infer I] ? I : never;
+type InferContra<T> = [T] extends [(arg: infer I) => void] ? I : never;
+type PickOne<T> = InferContra<InferContra<Contra<Contra<T>>>>;
+type UnionToTuple<T> = PickOne<T> extends infer U
+  ? Exclude<T, U> extends never
+    ? [T]
+    : [...UnionToTuple<Exclude<T, U>>, U]
+  : never;
+
+type LookupNumberOfOccurences<
+  Lookup extends unknown,
+  Arr extends unknown[],
+  R extends number = 0
+> = Arr extends [infer First, ...infer Rest]
+  ? First extends Lookup
+    ? LookupNumberOfOccurences<Lookup, Rest, Inc<R>>
+    : LookupNumberOfOccurences<Lookup, Rest, R>
+  : R;
+
+type CalculateScores<
+  Left extends unknown[],
+  Right extends unknown[],
+  R extends number[] = []
+> = Left extends [infer L1, ...infer LRest]
+  ? L1 extends number
+    ? CalculateScores<
+        LRest,
+        Right,
+        [...R, Mul<L1, LookupNumberOfOccurences<L1, Right>>]
+      >
+    : R
+  : R;
+
+//--
+type Input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+type Lines = SplitString<Input, '\n'>;
+type NumbersAsStringUnion = SplitString<Lines[number], '   '>;
+type NumberStrings = UnionToTuple<NumbersAsStringUnion>;
+type Numbers = StringDigitPairsToNumbers<NumberStrings>;
+
+type TransPosed = Transpose<Numbers>;
+type Left = TransPosed[0];
+type Right = TransPosed[1];
+
+type Scores = CalculateScores<Left, Right>;
+
+type Result = ArraySum<Scores>;
+//   ^?
