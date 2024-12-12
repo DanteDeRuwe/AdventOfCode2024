a3a8c9d792f51011d482d414367253b746d33ff6
Dante De Ruwe <dante.deruwe@ae.be>
2024-12-12 18:23:23 +0100

    Day 9.1

---
+++ C#/AdventOfCode2024/DayNine.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Immutable;
+
+// ReSharper disable once CheckNamespace
+namespace AdventOfCode2024.DayNine;
+
+public static class DayNine
+{
+    public static int PartOne(string input)
+    {
+        var allInput = input.Select(c => int.Parse(c.ToString()));
+
+        var isBlock = allInput.Index()
+            .GroupBy(x => x.Index % 2 == 0, x => x.Item)
+            .ToImmutableSortedDictionary(x => x.Key, x => x.AsEnumerable());
+
+        var freeSpaces = isBlock[false];
+        var blocks = isBlock[true].Index().Select(x => Enumerable.Repeat(x.Index, x.Item)).ToDeque();
+
+        var result = new List<int>();
+        foreach (var freeSpacesToFill in freeSpaces)
+        {
+            // Add first block
+            var first = blocks.RemoveFirst();
+            result.AddRange(first);
+
+            if (blocks.IsEmpty) break; // No more blocks to add
+
+            // Fill this free space with blocks from the end
+            var remainingSpacesToFill = freeSpacesToFill;
+            while (true)
+            {
+                var block = blocks.RemoveLast().ToArray();
+
+                if (block.Length > remainingSpacesToFill)
+                {
+                    var take = block.Take(remainingSpacesToFill);
+                    blocks.AddLast(block.Skip(remainingSpacesToFill)); // Add back the excess to the blocks
+                    result.AddRange(take);
+                    break; // Now no more free space to fill
+                }
+
+                result.AddRange(block);
+                remainingSpacesToFill -= block.Length;
+            }
+        }
+
+        return Checksum.Calculate(result);
+    }
+
+    public static int PartTwo(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class Checksum
+{
+    public static int Calculate(IEnumerable<int> numbers) => numbers.Index().Sum(x => x.Index * x.Item);
+}
+
+public record BlockItem(int Number, int Times);
+
+public class Deque<T>(IEnumerable<T>? items = null)
+{
+    private readonly LinkedList<T> _list = items is null ? new() : new(items);
+
+    public void AddFirst(T item) => _list.AddFirst(item);
+    public void AddLast(T item) => _list.AddLast(item);
+
+    public T RemoveFirst()
+    {
+        var first = _list.First;
+        _list.RemoveFirst();
+        return first.Value;
+    }
+
+    public T RemoveLast()
+    {
+        var last = _list.Last;
+        _list.RemoveLast();
+        return last.Value;
+    }
+
+    public T PeekFirst() => _list.First.Value;
+    public T PeekLast() => _list.Last.Value;
+    public int Count => _list.Count;
+    public bool IsEmpty => _list.Count == 0;
+}
+
+public static class Extensions
+{
+    public static Deque<T> ToDeque<T>(this IEnumerable<T> items) => new(items);
+}
---
+++ C#/Tests/DayNineTests.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode2024.DayNine;
+using FluentAssertions;
+
+namespace Tests;
+
+public class DayNineTests
+{
+    [Fact]
+    public void Test1()
+    {
+        const string input = "2333133121414131402";
+        DayNine.PartOne(input).Should().Be(1928);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        throw new NotImplementedException();
+    }
+}
